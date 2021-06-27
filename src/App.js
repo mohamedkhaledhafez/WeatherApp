61413f6a995069c268d651a5758b9d84e37a82c1
mohamedkhaledhafez <mohamedkhaled97858@gmail.com>
2021-06-28 01:58:24 +0200

    add all app files

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Form from "./components/Form/Form";
+import Weather from "./components/Weather/Weather";
+
+const API_KEY = "43f6ead6611327d30cc7fe81b124f617";
+//http://api.openweathermap.org/data/2.5/weather?q=cairo%2Cegypt&appid=e36ed364400282e43250b6c4c0274d44
+class App extends Component {
+  state = {
+    tempreature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    description: "",
+    error: "",
+  };
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}%2C,${country}&appid=${API_KEY}`
+    );
+
+    const data = await api.json();
+    console.log(data);
+    if (city && country) {
+      this.setState({
+        tempreature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: "",
+      });
+    } else {
+      this.setState({
+        tempreature: "",
+        city: "",
+        country: "",
+        humidity: "",
+        description: "",
+        error: "Please Enter City & Country",
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="main">
+        <div className="form-container">
+          <Form getWeather={this.getWeather} />
+          <Weather
+            tempreature={this.state.tempreature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
